397a85ddbdf9e173e1ef03830a117fa242cd1df1
vakror <vaylor2027@gmail.com>
2022-08-04 21:43:14 -0400

    Initial Commit

---
+++ First/FirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstApp.swift
+//  First
+//
+//  Created by Viktor on 48//22.
+//
+
+import SwiftUI
+
+@main
+struct FirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
